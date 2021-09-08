b44f8eebfda4111507e2549655cac6561f3bbef9
Isaiah Mazon <37008425+isamazon@users.noreply.github.com>
2021-09-07 18:53:55 -0600

    Added add book side bar route

--- client/src/components/reading/sidebar.js
+++ client/src/components/reading/sidebar.js
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 import { AiFillHome } from "react-icons/ai";
 import Booksvg from "../svgs/booksvg";
 import Readsvg from "../svgs/read";
+import Mag from "../svgs/add";
 // React reveal
 import Slide from "react-reveal/Slide";
 import "./profile.css";
@@ -12,7 +13,7 @@ function Sidebar() {
     <Col xl={1} className="sidebar-container">
       <Slide left>
         <div className="nav-side-bar">
-          <Link className="home-link">
+          <Link to="/" className="home-link">
             <AiFillHome className="home-icon" size="85%" />
           </Link>
           <ul className="nav-list">
@@ -30,6 +31,14 @@ function Sidebar() {
                 <Link to="/" className="link-area">
                   <Readsvg classname="svg" width="100%" height="100%" />
                   <p>Read </p>
+                </Link>
+              </li>
+            </Slide>
+            <Slide top>
+              <li className="nav-items">
+                <Link to="/" className="link-area">
+                  <Mag classname="svg" width="100%" height="80%" />
+                  <p>Add book</p>
                 </Link>
               </li>
             </Slide>
