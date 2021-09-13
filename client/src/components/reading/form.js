a3fcdad3b93a50a4da2e611ad6bba79ef0e29184
Isaiah Mazon <37008425+isamazon@users.noreply.github.com>
2021-09-13 14:11:13 -0600

    added some content to form component

--- client/src/components/reading/form.js
+++ client/src/components/reading/form.js
@@ -1,14 +1,30 @@
 import React, { useEffect, useState } from "react";
+// dependencies
+import FileBase from "react-file-base64";
+import { Col } from "react-bootstrap";
 // CSS
 import "./profile.css";
 function Form() {
   const [bookData, setBookData] = useState({
     title: "",
     author: "",
+    pageLength: "",
+    selectedFile: "",
   });
   return (
-    <div>
+    <div className="book-form-cont">
       <form className="form">
+        <h1 className="main-header">Upload your own book!</h1>
+        <div className="file-base-cnt">
+          <FileBase
+            type="file"
+            mutiple="false"
+            onDone={({ base64 }) =>
+              setBookData({ ...bookData, selectedFile: base64 })
+            }
+          />
+        </div>
+        {/* Book title */}
         <label>
           Book title:
           <input
@@ -16,13 +32,11 @@ function Form() {
             name="title"
             value={bookData.title}
             onChange={(e) =>
-              setBookData({
-                ...bookData,
-                title: e.target.value,
-              })
+              setBookData({ ...bookData, title: e.target.value })
             }
           />
         </label>
+        {/* author name */}
         <label>
           Authors name:
           <input
@@ -30,13 +44,25 @@ function Form() {
             name="author"
             value={bookData.author}
             onChange={(e) =>
-              setBookData({
-                ...bookData,
-                author: e.target.value,
-              })
+              setBookData({ ...bookData, author: e.target.value })
             }
           />
         </label>
+        {/* Page length */}
+        <label>
+          Page length
+          <input
+            type="text"
+            name="pageLength"
+            value={bookData.pageLength}
+            onChange={(e) =>
+              setBookData({ ...bookData, pageLength: e.target.value })
+            }
+          />
+        </label>
+        <button className="submit-button" type="submit">
+          Submit
+        </button>
       </form>
     </div>
   );
