b60b3a82b25dbe4d5fed163ad83e448a789fea32
Isaiah Mazon <37008425+isamazon@users.noreply.github.com>
2021-09-27 14:10:49 -0600

    Saving changes to form before starting over

--- client/src/components/login/login.js
+++ client/src/components/login/login.js
@@ -1,42 +1,97 @@
-import React from "react";
-import "./login.css";
-import Nav from "../navbar";
-import { Container, Row, Col } from "react-bootstrap";
-import Svg2 from "../svgs/reading2";
+import React, { useState } from 'react';
+import './login.css';
+import useStyles from './styles';
+import Input from './input';
+import { Grid } from '@material-ui/core';
+import { Button, Container } from 'react-bootstrap';
+import Svg2 from '../svgs/reading2';
 function Login() {
+  const [signUp, setSign] = useState('');
+  // Password toggle
+  const [showPassword, setShowPassword] = useState(false);
+  const handleShowPassword = () =>
+    setShowPassword((prevShowPassword) => !prevShowPassword);
+
+  const isSignUp = false;
+
+  // const handleSubmit = () => {};
+
+  // const handleChange = () => {};
+
+  const handleClick = () => {
+    setSign(!signUp);
+  };
+
+  const handleChange = () => {};
+
   return (
-    <div className="form-cont-main">
-      <Nav />
-      <Container fluid className="form-cont">
-        <Row className="row-1">
-          <Col className="col-1" lg={12}>
-            <div className="select-h1">
-              <h1 className="login-h1 selected-h1">Sign in</h1>
-              <h1 className="sign-h1">Sign up</h1>
-            </div>
-            {/* Form */}
-            <form className="form-1">
-              <div className="email-container">
-                <div className="email-input">
-                  <label className="custom-field-2">
-                    <input type="text" name="email" required />
-                    <span className="placeholder">Your Email</span>
-                  </label>
-                </div>
-              </div>
-              <div className="email-container">
-                <div className="email-input">
-                  <label className="custom-field-2">
-                    <input type="text" name="passowrd" required />
-                    <span className="placeholder">Password</span>
-                  </label>
-                </div>
-              </div>
-            </form>
-          </Col>
-        </Row>
-      </Container>
-    </div>
+    <Container fluid className="form-cont-main ">
+      <div className="form-container">
+        <form className="form" action="">
+          <h1>{signUp ? 'Sign up to blookify!' : 'Login to Blookify'}</h1>
+          <div className="signup-signin">
+            <p>
+              {signUp
+                ? 'Already have an account?'
+                : 'Dont have an account yet?'}
+            </p>
+            <p className="sign-p" onClick={handleClick}>
+              {signUp ? 'Sign in here' : 'Sign up here'}
+            </p>
+          </div>
+          <p className="or">--------- or ---------</p>
+          <Grid container spacing={2}>
+            {isSignUp && (
+              <>
+                <Input
+                  name="firstName"
+                  label="First Name"
+                  handleChange={handleChange}
+                  autoFocus
+                  half
+                />
+                <Input
+                  name="firstName"
+                  label="First Name"
+                  handleChange={handleChange}
+                  half
+                />
+              </>
+            )}
+            <Input
+              name="email"
+              label="Email Address"
+              handleChange={handleChange}
+              type="email"
+            />
+            <Input
+              name="password"
+              label="Password"
+              handleChange={handleChange}
+              type={showPassword ? 'text' : 'password'}
+              handleShowPassword={handleShowPassword}
+            />
+            {isSignUp && (
+              <Input
+                name=" confirmPassword"
+                label="Repeat password"
+                handleChange={handleChange}
+                type="password"
+              />
+            )}
+          </Grid>
+          {/* Submit button */}
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            className="button"
+          >
+            {isSignUp ? 'Sign up' : 'Sign in'}
+          </Button>
+        </form>
+      </div>
+    </Container>
   );
 }
 
