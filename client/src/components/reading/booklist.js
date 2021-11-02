6e1ffc64da128ceaf6c8b01732f61a4fc443159c
Isaiah Mazon <37008425+isamazon@users.noreply.github.com>
2021-11-02 12:36:06 -0600

    Updated and added a popup to the reading and read buttons

--- client/src/components/reading/booklist.js
+++ client/src/components/reading/booklist.js
@@ -1,9 +1,16 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import './booklist.css';
-import { FaCheckSquare } from 'react-icons/fa';
+import {
+  FaCheckSquare,
+  FaBook,
+  FaBookReader,
+  FaCaretDown,
+} from 'react-icons/fa';
+
 //
 import { createPost } from '../../actions/posts';
+import { createReadBook } from '../../actions/readposts';
 // img
 import pic1 from '../../assets/noimage.png';
 function Booklist(props) {
@@ -20,6 +27,20 @@ function Booklist(props) {
     e.preventDefault();
     console.log(bookData);
     dispatch(createPost(bookData));
+  };
+
+  const readSubmit = (e) => {
+    e.preventDefault();
+    dispatch(createReadBook(bookData));
+  };
+
+  const [popup, setPopup] = useState(false);
+  const [popup2, setPopup2] = useState(false);
+  const TogglePopup = () => {
+    setPopup(!popup);
+  };
+  const TogglePopup2 = () => {
+    setPopup2(!popup2);
   };
 
   return (
@@ -39,14 +60,37 @@ function Booklist(props) {
         Pg count: {props.pgcount ? props.pgcount : 'N/A'}
       </p>
       <div className="add-book">
-        <p>Click to add book to reading</p>
-        <FaCheckSquare
+        <p className={popup ? 'popup' : 'none'}>
+          Send to Reading
+          <span>
+            <FaCaretDown className="caretdown" />
+          </span>
+        </p>
+        <FaBookReader
+          onMouseEnter={TogglePopup}
+          onMouseLeave={TogglePopup}
           onClick={(e) => {
             handleSubmit(e);
             props.Toggle(e);
           }}
           className="check-mark"
-          size="40px"
+        />
+      </div>
+      <div className="add-book">
+        <p className={popup2 ? 'popup' : 'none'}>
+          Send to Read
+          <span>
+            <FaCaretDown className="caretdown" />
+          </span>
+        </p>
+        <FaBook
+          onMouseEnter={TogglePopup2}
+          onMouseLeave={TogglePopup2}
+          onClick={(e) => {
+            readSubmit(e);
+            props.Toggle(e);
+          }}
+          className="check-mark"
         />
       </div>
     </div>
